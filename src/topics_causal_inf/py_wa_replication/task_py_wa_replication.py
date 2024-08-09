7ab4fc646554cd3d0d6f5eee8a517d9ff4a628d3
Julian Budde <buddejul@gmail.com>
2024-08-09 09:23:46 +0200

    Add tables to WA 2018 replications using econml.

--- src/topics_causal_inf/py_wa_replication/task_py_wa_replication.py
+++ src/topics_causal_inf/py_wa_replication/task_py_wa_replication.py
@@ -5,11 +5,11 @@ from pathlib import Path
 from typing import Annotated, NamedTuple
 
 import numpy as np
+import pandas as pd  # type: ignore[import-untyped]
 from pytask import Product, task
-from wgan import DataWrapper, Generator  # type: ignore[import-untyped]
 
 from topics_causal_inf.classes import DGP
-from topics_causal_inf.config import BLD, RNG
+from topics_causal_inf.config import BLD, RNG, WGAN_GEN
 from topics_causal_inf.define_dgps import DGP3, DGP4, DGP5
 from topics_causal_inf.py_wa_replication.py_wa_replication import simulation
 
@@ -20,29 +20,30 @@ DIMS_TO_RUN = [8, 10, 12]
 class _Arguments(NamedTuple):
     dgp: DGP
     path_to_data: Path
+    data_generator: str
     n_sim: int = 20
     n_obs: int = 10_000
     dim: int = 10
-    data_generator: str = "standard"
-    generators: list[Generator] | None = None
-    data_wrappers: list[DataWrapper] | None = None
     rng: np.random.Generator = RNG
     num_trees: int = 100
     sample_fraction: float = 0.5
 
 
+DG_TO_RUN = ["standard", "wgan"]
+
 ID_TO_KWARGS = {
-    f"{dgp.name}_{dim}_standard": _Arguments(
+    f"{dgp.name}_{dim}_{dgen}": _Arguments(
         dgp=dgp,
         path_to_data=BLD
         / "py_wa_replication"
         / "sims"
-        / f"py_wa_replication_{dgp.name}_dim{dim}_standard.pkl",
+        / f"py_wa_replication_{dgp.name}_dim{dim}_{dgen}.pkl",
         dim=dim,
-        data_generator="standard",
+        data_generator=dgen,
     )
     for dgp in DGPS_TO_RUN
     for dim in DIMS_TO_RUN
+    for dgen in DG_TO_RUN
 }
 
 for id_, kwargs in ID_TO_KWARGS.items():
@@ -58,10 +59,21 @@ for id_, kwargs in ID_TO_KWARGS.items():
         rng: np.random.Generator,
         num_trees: int,
         sample_fraction: float,
-        generators: list[Generator] | None,
-        data_wrappers: list[DataWrapper] | None,
     ) -> None:
         """Task for WA 2018 replication simulations."""
+        if data_generator == "wgan":
+            wgan_train_data = pd.read_feather(WGAN_GEN / f"df_{dgp.name}.feather")
+            generators = pickle.load(
+                Path.open(WGAN_GEN / f"generators_{dgp.name}.pkl", "rb"),
+            )
+            data_wrappers = pickle.load(
+                Path.open(WGAN_GEN / f"data_wrapper_{dgp.name}.pkl", "rb"),
+            )
+        else:
+            wgan_train_data = None
+            generators = None
+            data_wrappers = None
+
         res = simulation(
             n_sim=n_sim,
             n_obs=n_obs,
@@ -71,6 +83,9 @@ for id_, kwargs in ID_TO_KWARGS.items():
             dgp=dgp,
             rng=rng,
             data_generator=data_generator,
+            wgan_train_data=wgan_train_data,
+            generators=generators,
+            data_wrappers=data_wrappers,
         )
 
         with Path.open(path_to_data, "wb") as f:
